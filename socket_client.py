34a31aaee7ca3b2bb0acb9ad98c68c1bef24d2b1
Pattadon Naksuwan <pattag26@gmail.com>
2022-08-17 23:11:09 +0700

    socket client

---
+++ socket_client.py
@@ -0,0 +1,9 @@
+import socket
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect(("192.168.1.115", 1234)) # "127.0.0.1" is a loopback ip address, which means you send the data to yourself and 1234 is the port number
+while True:
+    data = input('Choose the mode of the LED: ')
+    my_socket.sendall(bytes((data+"\n"), "ascii"))
+    if data == 'close':
+        break
+# you can use "nc -l 1234" to create a server to recieve the data (in this case type the command in the raspberry pi) or use socket_server.py to create a server too
