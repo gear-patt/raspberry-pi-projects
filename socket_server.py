93eb1760fd0d3a0ccd1b0fb778fcf0ee6dc725df
Pattadon Naksuwan <pattag26@gmail.com>
2022-08-17 23:13:55 +0700

    socket server

---
+++ socket_server.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import socket
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8,GPIO.OUT)
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.bind(("",1234))
+my_socket.listen(5)
+conn, addr = my_socket.accept()
+print('Got a request')
+
+while True:
+    print('new loop')
+    data = conn.recv(1000)
+    if data == b'on\n':
+        GPIO.output(8, True)
+        continue
+    if data == b'off\n':
+        GPIO.output(8, False)
+    if data == b'close\n':
+        break
+
+GPIO.cleanup()
+conn.close()
+my_socket.close()
+# you can use "nc ip_addr port_no" to send a data from any device (in this case can be your computer) to the server or you can use socket_client.py to send too
